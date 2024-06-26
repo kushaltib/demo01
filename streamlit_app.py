a88feab5dccda50526850f8ad3684b85a463ef1f
kushaltib <ktphdwork@gmail.com>
2024-06-26 15:21:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,26 @@
 import altair as alt
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import streamlit as st
 
-"""
-# See the change here now!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("NDC pledges for selected countries")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+col1,col2=st.columns([2,1])
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+with col1:
+    st.header("Choose the country")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+with col2:
+    country_options=("India","EU27","China")
+    selected_country= st.selectbox(options=country_options)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+if selected_country=="India":
+    st.markdown(textwrap.dedent("""\
+                                India
+                                """))
+else:
+    print("No")
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
