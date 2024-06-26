7e3be7a87bd02c10546704e2588e2f4c81f16b03
kushaltib <ktphdwork@gmail.com>
2024-06-26 14:40:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# See the change here today!
+# See the change here now!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
