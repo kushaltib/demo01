2c2ab6e7bc1f259cf02f762dfadb1745aab365ee
kushaltib <ktphdwork@gmail.com>
2024-08-05 11:18:05 +0200

    Update mod_read_input.py

--- model/mod_read_input.py
+++ model/mod_read_input.py
@@ -20,7 +20,12 @@ def read_ndc():
     
     #--read the NDC file
     #NDC = pd.read_excel("./data/NDCdata_As12Jun2024_Upd12Jun2024_v2.xlsx")
-    NDC = pd.read_excel("./data/NDCdata_As12Jun2024_Upd29Jul2024_v2.xlsx")
+    #NDC = pd.read_excel("./data/NDCdata_As12Jun2024_Upd29Jul2024_v2.xlsx")
+    NDC = pd.read_excel("./data/NDCdata_As12Jun2024_Upd29Jul2024_v2_othercountries.xlsx")
+
+
+
+
     NDC.drop(columns=['Categories'],inplace=True)
     NDC = NDC.T
     header_col = NDC.iloc[0]
